7cc4edfa3785224a2fdea2d548ee46bfd5b12830
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-02-21 13:40:27 +0100

    fix

--- Mathlib/Init/Core.lean
+++ Mathlib/Init/Core.lean
@@ -3,10 +3,10 @@ Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Mario Carneiro
 -/
-import Mathlib.Mathport.Rename
-import Std.Classes.SetNotation
 import Std.Tactic.Relation.Rfl
 import Std.Tactic.Relation.Symm
+import Std.Util.ExtendedBinder
+import Mathlib.Mathport.Rename
 import Mathlib.Tactic.Relation.Trans
 
 /-!
