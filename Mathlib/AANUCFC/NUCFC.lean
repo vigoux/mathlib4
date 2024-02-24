0db54a1f493f44f2c8a654f5d2dcd189ff268bcf
Jireh Loreaux <loreaujy@gmail.com>
2024-02-24 15:16:31 -0600

    work

--- Mathlib/AANUCFC/NUCFC.lean
+++ Mathlib/AANUCFC/NUCFC.lean
@@ -1,13 +1,33 @@
 import Mathlib.Topology.ContinuousFunction.FunctionalCalculus
 import Mathlib.Algebra.Algebra.Unitization
 
+namespace Unitization
+
+variable {R A : Type*} [CommSemiring R] [NonUnitalSemiring A] [Module R A] [IsScalarTower R A A]
+  [SMulCommClass R A A]
+
+lemma isUnit_fst {x : Unitization R A} (hx : IsUnit x) : IsUnit x.fst :=
+  hx.map <| fstHom R A
+
+lemma isUnit_of_fst_one (x : (Unitization R A)ˣ) (hx_fst : x.val.fst = 1) :
+    x.val.snd + x⁻¹.val.snd + x.val.snd * x⁻¹.val.snd = 0 := by
+  have := congr($(x.mul_inv).snd)
+  simp [-Units.mul_inv, snd_one, hx_fst] at this
+
+  sorry
+
+#exit
+
+end Unitization
 namespace v1
 
 def quasiSpectrum (R : Type*) {A : Type*} [Semifield R] [NonUnitalRing A] [Module R A]
     (a : A) : Set R :=
-  { r : R | ∃ b, r⁻¹ • a + b - r⁻¹ • a * b = 0 ∧ r⁻¹ • a + b - b * r⁻¹ • a = 0 }
+  { r : R | ¬ ∃ b, r⁻¹ • a + b = r⁻¹ • a * b ∧ r⁻¹ • a + b = b * r⁻¹ • a }
 
 variable (R : Type*) {A : Type*} [Semifield R] [Ring A] [Algebra R A]
+
+namespace quasiSpectrum
 
 lemma quasiSpectrum_eq (a : A) : quasiSpectrum R a = spectrum R a := by
   ext r
@@ -20,6 +40,7 @@ lemma quasiSpectrum_eq (a : A) : quasiSpectrum R a = spectrum R a := by
 
 -- 1 = (1 - r⁻¹ • a) * b = b - r⁻¹ • a * b
 -- set b = 1 - c, then we find 1 = (1 - c) - r⁻¹ • a * (1 - c) = 1 - c - r⁻¹ • a + r⁻¹ • a * c
+
 
 -- so, if `b` is the unit in `A₁`, then `1 - c` is the unit in `A`.
 
