d50b598a1d5ace7d7b45bb53aed7b57f33da7127
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-06-27 10:12:12 +0200

    wip

--- Mathlib/Sandbox.lean
+++ Mathlib/Sandbox.lean
@@ -3,10 +3,21 @@ import Mathlib.MeasureTheory.Constructions.Pi
 
 theorem MeasureTheory.measurePreserving_pi {ι : Type*} [Fintype ι] {α : ι → Type*} {β : ι → Type*}
     [∀ i, MeasurableSpace (α i)] [∀ i, MeasurableSpace (β i)] (μ : (i : ι) → Measure (α i))
-    (ν : (i : ι) → Measure (β i)) [∀ i, SFinite (μ i)] [∀ i, SFinite (ν i)]
-    {f : (i : ι) → (α i) → (β i)} (hf : ∀ i, MeasurePreserving (f i) (μ i) (ν i)) :
-    MeasurePreserving (fun a i ↦ f i (a i)) (Measure.pi μ) (Measure.pi ν)  := by
-  sorry
+    (ν : (i : ι) → Measure (β i)) [∀ i, SigmaFinite (μ i)] [∀ i, SigmaFinite (ν i)]
+    {f : (i : ι) → (α i) ≃ᵐ (β i)} (hf : ∀ i, MeasurePreserving (f i) (μ i) (ν i)) :
+    MeasurePreserving (fun a i ↦ f i (a i)) (Measure.pi μ) (Measure.pi ν) := by
+  convert ((MeasurableEquiv.piCongrRight f).symm.measurable.measurePreserving (Measure.pi ν)).symm
+  refine Measure.pi_eq fun s hs ↦ ?_
+  rw [MeasurableEquiv.map_symm, Measure.comap_apply]
+  simp_rw [MeasurableEquiv.image_eq_preimage, MeasurableEquiv.piCongrRight, Equiv.piCongrRight,
+    MeasurableEquiv.coe_toEquiv, MeasurableEquiv.coe_toEquiv_symm, MeasurableEquiv.symm_mk,
+    MeasurableEquiv.coe_mk, Equiv.coe_fn_symm_mk, Set.preimage_pi,
+    ← MeasurableEquiv.image_eq_preimage, Measure.pi_pi,
+    ← MeasurePreserving.measure_preimage (hf _)
+    ((MeasurableEquiv.measurableSet_image (f _)).mpr (hs _)), MeasurableEquiv.preimage_image]
+  · exact (MeasurableEquiv.piCongrRight f).injective
+  · exact fun _ hs ↦ ((MeasurableEquiv.piCongrRight f).measurableSet_image ).mpr hs
+  · exact MeasurableSet.univ_pi hs
 
 theorem Real.rpow_ne_zero_of_pos {x : ℝ} (hx : 0 < x) (y : ℝ) : x ^ y ≠ 0 := by
   rw [rpow_def_of_pos hx]; apply exp_ne_zero _
