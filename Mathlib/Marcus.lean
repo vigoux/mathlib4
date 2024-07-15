e8d64a3a0164d21acb20c9106c1151ff30b4327a
Xavier Roblot <roblot@math.univ-lyon1.fr>
2024-07-15 17:32:04 +0200

    wip

--- Mathlib/Marcus.lean
+++ Mathlib/Marcus.lean
@@ -31,9 +31,12 @@ variable (K)
 def mapToUnitsPow₀ (c : {w : InfinitePlace K // w ≠ w₀} → ℝ) : InfinitePlace K → ℝ :=
   fun w ↦ ∏ i, w (fundSystem K (equivFinRank i)) ^ (c i)
 
+theorem mapToUnitsPow₀_pos (c : {w : InfinitePlace K // w ≠ w₀} → ℝ) (w : InfinitePlace K) :
+    0 < mapToUnitsPow₀ K c w := sorry
+
 local notation "F" K => (InfinitePlace K → ℝ) × ({w : InfinitePlace K // IsComplex w} → ℝ)
 
-@[simps source target]
+@[simps apply source target]
 def mapToUnitsPow : PartialHomeomorph (InfinitePlace K → ℝ) (InfinitePlace K → ℝ) where
   toFun := fun c ↦ |c w₀| ^ (finrank ℚ K : ℝ)⁻¹ • mapToUnitsPow₀ K (fun w ↦ c w)
   invFun := sorry
@@ -47,6 +50,21 @@ def mapToUnitsPow : PartialHomeomorph (InfinitePlace K → ℝ) (InfinitePlace K
   open_target := sorry
   continuousOn_toFun := sorry
   continuousOn_invFun := sorry
+
+theorem mapToUnitsPow_nonneg (c : InfinitePlace K → ℝ) (w : InfinitePlace K) :
+    0 ≤ mapToUnitsPow K c w  := by
+  rw [mapToUnitsPow_apply, Pi.smul_apply, smul_eq_mul]
+  refine mul_nonneg ?_ ?_
+  · exact Real.rpow_nonneg (abs_nonneg _) _
+  · exact (mapToUnitsPow₀_pos _ _ _).le
+
+theorem mapToUnitsPow_zero_iff (c : InfinitePlace K → ℝ) (w : InfinitePlace K) :
+    mapToUnitsPow K c w = 0 ↔ c w₀ = 0 := by
+  simp_rw [mapToUnitsPow_apply, Pi.smul_apply, smul_eq_mul, mul_eq_zero,
+    ne_of_gt (mapToUnitsPow₀_pos _ _ _), or_false, Real.rpow_eq_zero_iff_of_nonneg (abs_nonneg _),
+    abs_eq_zero, and_iff_left_iff_imp, ne_eq, inv_eq_zero, Nat.cast_eq_zero, ← ne_eq]
+  intro _
+  exact ne_of_gt finrank_pos
 
 @[simps! apply symm_apply source target]
 def marcus₁ : PartialHomeomorph (F K) (F K) :=
@@ -95,6 +113,13 @@ theorem marcus₃_apply (x : F K) :
     marcus₂_apply, PartialHomeomorph.pi, PartialHomeomorph.symm_toPartialEquiv,
     PartialHomeomorph.mk_coe, PartialEquiv.pi_apply, PartialHomeomorph.coe_coe_symm]
 
+@[simp]
+theorem marcus₃_symm_apply (x : E K) :
+    (marcus₃ K).symm x =
+      ⟨fun w ↦ if hw : IsReal w then x.1 ⟨w, hw⟩ else
+        ‖x.2 ⟨w, not_isReal_iff_isComplex.mp hw⟩‖, fun w ↦ Complex.arg (x.2 w)⟩ := by
+  simp [marcus₃, Complex.polarCoord, Complex.abs_eq_sqrt_sq_add_sq]
+
 theorem marcus₃_source : (marcus₃ K).source =
     (Set.univ.pi fun w ↦
       if IsReal w then Set.univ else Set.Ioi 0) ×ˢ (Set.univ.pi fun _ ↦ Set.Ioo (-π) π):= by
@@ -119,6 +144,66 @@ theorem marcus₃_target :
 
 def full_marcus : PartialHomeomorph (F K) (E K) := by
   refine PartialHomeomorph.trans (marcus₁ K) (marcus₃ K)
+
+theorem full_marcus_source :
+    (full_marcus K).source =
+      (Set.univ.pi fun i ↦ if i = w₀ then Set.Ioi 0 else Set.univ) ×ˢ
+        Set.univ.pi fun _ ↦ Set.Ioo (-π) π := by
+  simp only [full_marcus, PartialHomeomorph.trans_toPartialEquiv, PartialEquiv.trans_source,
+    marcus₁_source, PartialHomeomorph.toFun_eq_coe]
+  let U : Set ℝ := if ∃ w : InfinitePlace K, IsComplex w then {0}ᶜ else Set.univ
+  have : (marcus₁ K) ⁻¹' (marcus₃ K).source =
+      (Set.univ.pi fun w ↦ if w = w₀ then U else Set.univ) ×ˢ
+        (Set.univ.pi fun _ ↦ Set.Ioo (-π) π) := by
+    ext x
+    simp_rw [marcus₃_source, Set.mem_preimage, marcus₁_apply, Set.mem_prod, Set.mem_pi,
+      Set.mem_univ, true_implies, Set.mem_ite_univ_left, not_isReal_iff_isComplex,
+      and_congr_left_iff, Set.mem_ite_univ_right, Set.mem_Ioi, lt_iff_le_and_ne,
+      mapToUnitsPow_nonneg, true_and, ne_comm (a := (0:ℝ)), ne_eq, mapToUnitsPow_zero_iff]
+    simp_rw [forall_eq]
+    intro _
+    dsimp only [U]
+    by_cases hc : ∃ w : InfinitePlace K, IsComplex w
+    · obtain ⟨w, hw⟩ := hc
+      have : (∀ (z : InfinitePlace K), z.IsComplex → ¬ x.1 w₀ = 0) ↔ x.1 w₀ ≠ 0 :=
+        ⟨fun h ↦ h w hw, fun h _ _ ↦ h⟩
+      rw [this, if_pos, Set.mem_compl_iff, Set.mem_singleton_iff]
+      exact ⟨w, hw⟩
+    · have : (∀ (z : InfinitePlace K), z.IsComplex → ¬ x.1 w₀ = 0) := by
+        rw [not_exists] at hc
+        exact fun z a _ ↦ hc z a
+      simp [this]
+  rw [this]
+  rw [Set.prod_inter_prod, Set.univ_inter]
+  rw [← Set.pi_inter_distrib]
+  congr! 3
+  dsimp only [U]
+  split_ifs <;> aesop
+
+theorem full_marcus_target :
+    (full_marcus K).target =
+      (Set.univ.pi fun _ ↦ Set.Ioi 0) ×ˢ (Set.univ.pi fun _ ↦ Complex.slitPlane) := by
+  simp_rw [full_marcus, PartialHomeomorph.trans_toPartialEquiv, PartialEquiv.trans_target,
+    marcus₃_target, PartialHomeomorph.coe_coe_symm, marcus₁_target]
+  have : (marcus₃ K).symm ⁻¹' (Set.univ.pi fun x ↦ Set.Ioi 0) ×ˢ Set.univ =
+      (Set.univ.pi fun _ ↦ Set.Ioi 0) ×ˢ (Set.univ.pi fun _ ↦ {0}ᶜ) := by
+    ext
+    simp_rw [Set.mem_preimage, marcus₃_symm_apply, Set.mem_prod, Set.mem_pi, Set.mem_univ,
+      true_implies, and_true, Set.mem_Ioi, Set.mem_compl_iff, Set.mem_singleton_iff]
+    refine ⟨fun h ↦ ⟨fun w ↦ ?_, fun w ↦ ?_⟩, fun h w ↦ ?_⟩
+    · have := h w
+      rwa [dif_pos w.prop] at this
+    · have := h w
+      rwa [dif_neg (not_isReal_iff_isComplex.mpr w.prop), norm_pos_iff] at this
+    · by_cases hw : IsReal w
+      · rw [dif_pos hw]
+        exact h.1 ⟨w, hw⟩
+      · rw [dif_neg hw, norm_pos_iff]
+        exact h.2 ⟨w, not_isReal_iff_isComplex.mp hw⟩
+  rw [this, Set.prod_inter_prod, Set.univ_inter]
+  rw [← Set.pi_inter_distrib]
+  have : Complex.slitPlane ∩ {0}ᶜ = Complex.slitPlane := by simp
+  simp_rw [this]
 
 def normVector : (E K) → (InfinitePlace K → ℝ) := fun x w ↦ normAtPlace w x
 
@@ -173,6 +258,7 @@ theorem image_by_marcus₃ (s : Set (E K)) (h₁ : ∀ x ∈ s, ∀ w, 0 ≤ x.1
     s = marcus₃ K '' ((normVector K '' s) ×ˢ Set.univ.pi fun _ ↦ Set.Ioc (-π) π) := by
   rw [marcus₃_prod_pi]
   sorry
+  sorry
 
 def box₁ : Set (InfinitePlace K → ℝ) :=
   Set.univ.pi fun w ↦ if w = w₀ then Set.Ioc 0 1 else Set.Ico 0 1
@@ -187,18 +273,69 @@ def normLessThanOnePlus : (Set (E K)) := (normLessThanOne K) ∩ {x | ∀ w, 0 <
 theorem eval₀ :
     mapToUnitsPow K '' (box₁ K) = normVector K '' (normLessThanOnePlus K ) := sorry
 
-theorem eval :
-    full_marcus K '' (box K) = normLessThanOnePlus K := by
+theorem normLessThanOnePlus_eq_image :
+    normLessThanOnePlus K  = full_marcus K '' (box K) := by
   unfold full_marcus box
   simp_rw [PartialHomeomorph.coe_trans, Set.image_comp, marcus₁_image_prod]
   rw [box₂, eval₀]
   convert (image_by_marcus₃ K (normLessThanOnePlus K) ?_ ?_).symm
   · sorry
   · sorry
+  · sorry
+  · sorry
 
-theorem volume_marcus₃_prod_box₂ (s : Set (InfinitePlace K → ℝ)) :
-    volume (marcus₃ K '' (s ×ˢ box₂ K)) = (2 * NNReal.pi) ^ NrComplexPlaces K *
+-- open ENNReal in
+-- private theorem volume_marcus₃_set_prod_set_aux
+--     (f : (InfinitePlace K → ℝ) × ({w : InfinitePlace K // w.IsComplex} → ℝ) → ℝ≥0∞)
+--     (W : Finset {w : InfinitePlace K // w.IsComplex}) (hf : Measurable f)
+--     (a : {w : InfinitePlace K // w.IsComplex} → ℂ) (x : {w : InfinitePlace K // w.IsReal} → ℝ) :
+--     (∫⋯∫⁻_W, fun y ↦ f ⟨x, fun w ↦ ‖y w‖⟩ ∂fun _ ↦ (volume : Measure ℂ)) a =
+--       (2 * NNReal.pi) ^ W.card * (∫⋯∫⁻_W, (fun y ↦ (∏ i ∈ W, (y i).toNNReal) * f ⟨x, y⟩)
+--         ∂fun _ ↦ (volume.restrict (Set.Ici (0 : ℝ)))) (fun i ↦ ‖a i‖) := by
+--   sorry
+
+-- Prove the full_marcus version below instead?
+theorem volume_marcus₃_set_prod_set (s : Set (InfinitePlace K → ℝ))
+    (t : Set ({w : InfinitePlace K // IsComplex w} → ℝ)) :
+    volume (marcus₃ K '' (s ×ˢ t)) = volume t *
       ∫⁻ x in s, ∏ w : {w : InfinitePlace K // IsComplex w}, (x w).toNNReal:= by
+  rw [← setLIntegral_one, ← lintegral_indicator, Measure.volume_eq_prod, lintegral_prod]
+  let sr : Set ({w : InfinitePlace K // IsReal w} → ℝ) := {x | ∃ a ∈ s, ∀ w, x w = a w}
+  let sc : Set ({w : InfinitePlace K // IsComplex w} → ℝ × ℝ) := sorry
+  have : ∀ x y,
+      (marcus₃ K '' s ×ˢ t).indicator (fun _ ↦ (1 : ℝ≥0∞)) (x, y) =
+        sr.indicator 1 x * sc.indicator 1 (fun w ↦ Complex.polarCoord (y w)) := by
+    intro x y
+    by_cases h : (x, y) ∈ marcus₃ K '' (s ×ˢ t)
+    · rw [Set.indicator_of_mem h, Set.indicator_of_mem, Set.indicator_of_mem, Pi.one_apply,
+        Pi.one_apply, one_mul]
+      · simp at h
+
+        sorry
+      · simp at h
+        obtain ⟨a, _, ⟨ha₁, _⟩, rfl, _⟩ := h
+        refine ⟨a, ha₁, ?_⟩
+        intro _
+        rfl
+    · sorry
+  simp_rw [this]
+  simp_rw [lintegral_const_mul' _ _ sorry]
+  
+
+  sorry
+
+theorem volume_full_marcus_set_prod_set (s : Set (InfinitePlace K → ℝ))
+    (t : Set ({w : InfinitePlace K // IsComplex w} → ℝ)) :
+    volume (full_marcus K '' (s ×ˢ t)) =
+    volume t * ∫⁻ x in mapToUnitsPow K '' s,
+      ∏ w : { w : InfinitePlace K // w.IsComplex }, (x w).toNNReal := by
+  rw [← setLIntegral_one, ← lintegral_indicator, Measure.volume_eq_prod, lintegral_prod]
+  rw [full_marcus, PartialHomeomorph.coe_trans, Set.image_comp, marcus₁_image_prod]
+  rw [marcus₃]
+  simp only [PartialHomeomorph.coe_trans, PartialHomeomorph.prod_apply,
+    PartialHomeomorph.refl_apply, id_eq, Homeomorph.toPartialHomeomorph_apply, Function.comp_apply,
+    marcus₂_apply]
+
   sorry
 
 def jacobianCoeff (w i : InfinitePlace K) : (InfinitePlace K → ℝ) → ℝ :=
@@ -229,11 +366,51 @@ theorem jacobian_det (c : InfinitePlace K → ℝ) :
 
 theorem volume_normLessThanOnePlus :
     (volume (normLessThanOnePlus K)).toReal = π ^ NrComplexPlaces K * regulator K := by
-  simp_rw [← eval, full_marcus, PartialHomeomorph.coe_trans, box]
-  simp_rw [Set.image_comp, marcus₁_image_prod]
-  rw [volume_marcus₃_prod_box₂]
+  rw [normLessThanOnePlus_eq_image, box, volume_full_marcus_set_prod_set]
   rw [lintegral_image_eq_lintegral_abs_det_fderiv_mul volume sorry
      (fun c _ ↦ HasFDerivAt.hasFDerivWithinAt (hasFDeriv_mapToUnitsPow K c))
      (injOn_mapToUnitsPow K)]
   simp_rw [jacobian_det]
+  sorry
+
+theorem closure_subset_closure :
+    closure (normLessThanOnePlus K) ⊆ full_marcus K '' (closure (box K)) := by
+  refine closure_minimal ?_ ?_
+  · rw [normLessThanOnePlus_eq_image]
+    refine Set.image_mono ?_
+    exact subset_closure
+  · have t₁ : IsCompact (closure (box K)) := sorry
+    have t₂ : ContinuousOn (full_marcus K) (closure (box K)) := sorry
+    have := t₁.image_of_continuousOn t₂
+    exact IsCompact.isClosed this
+
+theorem box_subset_source :
+    (box K) ⊆ (full_marcus K).source := sorry
+
+theorem normLessThanOnePlus_subset_target :
+    normLessThanOnePlus K ⊆ (full_marcus K).target := sorry
+
+theorem interior_eq_interior :
+    full_marcus K '' (interior (box K)) = interior (normLessThanOnePlus K) := by
+  have : (full_marcus K).IsImage (box K) (normLessThanOnePlus K) := sorry
+  have := this.interior
+  have := PartialHomeomorph.IsImage.image_eq this
+  rwa [Set.inter_eq_right.mpr, Set.inter_eq_right.mpr] at this
+  · refine subset_trans ?_ (normLessThanOnePlus_subset_target K)
+    exact interior_subset
+  · refine subset_trans ?_ (box_subset_source K)
+    exact interior_subset
+
+theorem volume_frontier :
+    volume (frontier (normLessThanOnePlus K)) = 0 := by
+  rw [frontier]
+  have := Set.diff_subset_diff_left ( closure_subset_closure K)
+    (t := interior (normLessThanOnePlus K))
+  rw [← nonpos_iff_eq_zero]
+  refine le_trans (measure_mono this) ?_
+  rw [← interior_eq_interior]
+  refine le_trans (measure_mono (Set.subset_image_diff _ _ _)) ?_
+  rw [closure_diff_interior]
+  rw [box]
+  rw [frontier_prod_eq]
   sorry
