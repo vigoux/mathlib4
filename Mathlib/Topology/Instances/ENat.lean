071aeed9b97be2d6d97a35443bf89a2ab5c56097
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-07-26 14:13:18 +0200

    Change imports

--- Mathlib/Topology/Instances/ENat.lean
+++ Mathlib/Topology/Instances/ENat.lean
@@ -3,8 +3,9 @@ Copyright (c) 2024 Etienne Marion. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Etienne Marion
 -/
-import Mathlib.Data.ENat.Lattice
-import Mathlib.Topology.Algebra.Order.Compact
+import Mathlib.Data.Nat.Lattice
+import Mathlib.Data.ENat.Basic
+import Mathlib.Topology.Order.Basic
 
 /-!
 # Topology on extended natural numbers
