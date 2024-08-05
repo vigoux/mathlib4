f3d8847d9d4e5765d6ffe6e9d9fd02a176f4d08b
Eric Wieser <wieser.eric@gmail.com>
2024-08-05 12:12:59 +0000

    repeat the rename from master

--- test/algebra_rat.lean
+++ test/algebra_rat.lean
@@ -1,7 +1,7 @@
 import Mathlib.Algebra.Algebra.Rat
 
 /-- The two `Algebra ℚ≥0 ℚ≥0` instances should coincide. -/
-example : NNRat.instAlgebra = Algebra.id ℚ≥0 := rfl
+example : NNDivisionRing.toRatAlgebra = Algebra.id ℚ≥0 := rfl
 
 /-- The two `Algebra ℚ ℚ` instances should coincide. -/
-example : Rat.instAlgebra = Algebra.id ℚ := rfl
+example : DivisionRing.toRatAlgebra = Algebra.id ℚ := rfl
